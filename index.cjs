d50fb10212b0b772019e7bd24ee167d2a6b8512a
RuyOllivere <ruylivinginadream@duck.com>
2025-10-04 09:18:12 -0300

    Initial commit - backend chat server

---
+++ index.cjs
@@ -0,0 +1,38 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
